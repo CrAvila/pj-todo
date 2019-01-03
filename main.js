4dd09c850587f03e40b387f8b63e180b648c6821
CR_Avila <carlosrobertoavilah@gmail.com>
2019-01-02 19:54:13 -0600

    Update to V1

---
+++ main.js
@@ -0,0 +1,22 @@
+// V1 requirements 
+    // It should have a place to store todos
+
+    let toDos = ['Item 1', 'Item 2', 'Item 3'];
+
+    // It should have a way to display todos
+
+    console.log(toDos);
+
+    // It should have a way to add new todos
+
+    toDos.push('Item 4');
+    toDos.push('Item 5');
+
+    // It should have a way to change a todos
+
+    toDos[0] = 'Changed!';
+
+    // It should have a way to delete todo
+
+    toDos.splice(0, 1);
+
